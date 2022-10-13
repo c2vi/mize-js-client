24878ddddee86bd026a049ac74dc3add4cdb6ac4
Sebastian Moser <sewi.moser@gmail.com>
2022-10-13 19:09:12 +0200

    getting the id that is in the pathname now works

--- src/main.js
+++ src/main.js
@@ -1,18 +1,39 @@
 
 pr = console.log
 
-async function main(so){
+let render_classes = []
+let renders= []
 
+async function main(so){
+	//get item
+	
 	//get render
 	let first = await import("/$api/render/first")
 	first = first.First
+
+	render_classes.push({
+		id: "first",
+		ob: first,
+	})
+	let id = parseInt(location.pathname.slice(1))
+	if (location.pathname == "/") {id = 1}
+	if (id == NaN) {pr("id is NaN"); id = 0}
+
 	customElements.define("mize-first", first);
 	const mize = document.getElementById("mize")
 	const item_element = document.createElement("mize-first");
+	renders.push({
+		render_id: "first",
+		ob: item_element,
+		item_id: id,
+	})
+	item_element.so = so
 	mize.appendChild(item_element)
 
-	let num_u8 = new Uint8Array([1,1,0,0,0,0,0,0,0,0])
+	let num_u8 = new Uint8Array([1,1])
+	num_u8 = new Uint8Array([...num_u8, ...u64_to_be_bytes(id)])
 	so.send(num_u8)
+
 }
 
 
@@ -30,8 +51,12 @@ async function handle_message(message){
 		case 2:
 			const id = from_be_bytes(message.slice(2, 10))
 			const num_of_fields = from_be_bytes(message.slice(10, 14))
-			let item_u8 = []
-			let item = []
+			let item = {}
+			item.u8 = []
+			item.str = []
+			item.test = "testing"
+			item.testfunc = function(){
+			}
 
 			let index = 14
 			let fields = 0
@@ -47,21 +72,19 @@ async function handle_message(message){
 				const key_str = String.fromCharCode.apply(null, key);
 				const val_str = String.fromCharCode.apply(null, val);
 
-				item_u8.push([key, val])
-				item.push([key_str, val_str])
+				item.u8.push([key, val])
+				item.str.push([key_str, val_str])
 
 				index += 4 + val_len
 
 				fields += 1
 			}
-			for (f of item) {
-				//console.log(f)
-				console.log("Key: ", f[0])
-				console.log("Val: ", f[1])
-			}
 
-			//console.log("num_of_fields: " + num_of_fields)
-			//console.log("ID: " , id)
+			//set item on render
+			let [render] = renders.filter( (render) => render.item_id == id)
+			render.ob.getItemCallback(item)
+
+			
 			break;
 
 		case 3:
@@ -110,10 +133,12 @@ document.addEventListener("DOMContentLoaded", () =>{
 })
 
 function from_be_bytes(bytes){
-	bytes = bytes.reverse()
+	clone = Array.from(bytes)
+	clone.reverse()
+
 	let count = 0
 	let num = 0
-	for (i of bytes) {
+	for (i of clone) {
 		num += i * 256 ** count
 		count += 1
 	}
