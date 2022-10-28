eec5e0999a01dff3eb52522caf70c0d0fd0832de
Sebastian Moser <sewi.moser@gmail.com>
2022-10-28 17:23:04 +0200

    cleanup: now a class for Item and Field

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 
+//some global stuff
 pr = console.log
+mize = {}
+mize.encoder = new TextEncoder()
+mize.decoder = new TextDecoder()
 
 let render_classes = []
 let renders= []
 
+document.addEventListener("DOMContentLoaded", () =>{
+	const so = new WebSocket("ws://localhost:3000/$api/socket")
+	so.onopen = () => {
+		so.onmessage = async (message) => {
+			handle_message(new Uint8Array(await message.data.arrayBuffer()))
+		}
+		main(so)
+	}
+})
+
 async function main(so){
-	//get item
-	
 	//get render
 	let first = await import("/$api/render/first")
 	first = first.First
@@ -36,27 +48,57 @@ async function main(so){
 
 }
 
+class Item{
+	constructor(id, raw){
+		//raw: [["key1", "val1"]["key2", "val2"]]
+		this.fields = []
+		for (let field_raw of raw){
+			this.fields.push(new Field(field_raw))
+		}
+		this.id = id
+	}
+}
 
+class Field{
+	constructor(raw){
+		//raw: ["key", "val"]
+		this.raw = raw
+	}
+	get str(){
+		return [String.fromCharCode.apply(null, this.raw[0]), String.fromCharCode(null, this.raw[1])]
+	}
+
+	get key(){
+	
+		return mize.decoder.decode(this.raw[0])
+	}
+
+	get val(){
+		return mize.decoder.decode(this.raw[1])
+	}
+
+	get val_as_number(){
+		return from_be_bytes(this.raw[1])
+	}
+
+	get val_raw(){
+		return this.raw[1]
+	}
+
+}
 async function handle_message(message){	
 	const version = message[0]
 	const cmd = message[1]
-
-	//console.log("CMD: " + cmd)
-	//console.log("MSG: " + message)
 
 	switch(cmd){
 		case 1:
 			break;
 
 		case 2:
-			const id = from_be_bytes(message.slice(2, 10))
+			let id = from_be_bytes(message.slice(2, 10))
 			const num_of_fields = from_be_bytes(message.slice(10, 14))
-			let item = {}
-			item.u8 = []
-			item.str = []
-			item.test = "testing"
-			item.testfunc = function(){
-			}
+
+			let raw = []
 
 			let index = 14
 			let fields = 0
@@ -69,11 +111,7 @@ async function handle_message(message){
 				const val_len = from_be_bytes(message.slice(index + 0, index + 4))
 				const val = message.slice(index + 4, index + 4 + val_len)
 
-				const key_str = String.fromCharCode.apply(null, key);
-				const val_str = String.fromCharCode.apply(null, val);
-
-				item.u8.push([key, val])
-				item.str.push([key_str, val_str])
+				raw.push([key, val])
 
 				index += 4 + val_len
 
@@ -82,7 +120,7 @@ async function handle_message(message){
 
 			//set item on render
 			let [render] = renders.filter( (render) => render.item_id == id)
-			render.ob.getItemCallback(item)
+			render.ob.getItemCallback(new Item(id, raw))
 
 			
 			break;
@@ -109,6 +147,98 @@ async function handle_message(message){
 			break;
 
 		case 10:
+			decoder = new TextDecoder()
+			let id_update = from_be_bytes(message.slice(2,10))
+			let num_of_updates = from_be_bytes(message.slice(10, 14))
+			let i = 0
+			let index_update = 14
+			const [render_update] = renders.filter( render => render.item_id == id_update)
+			let new_item = {
+				u8: Array.from(render_update.ob.item.u8),
+				str: Array.from(render_update.ob.item.str),
+			}
+			while (i < num_of_updates) {
+
+				//get key_len
+				let key_len = from_be_bytes(message.slice(index_update, index_update + 4))
+				index_update += 4
+
+				//get key
+				let key = new Uint8Array(message.slice(index_update, index_update + key_len))
+				let key_str = decoder.decode(key)
+				index_update += key_len
+
+				//get update_len
+				let update_len = from_be_bytes(message.slice(index_update, index_update + 4))
+				
+				//apply for key
+				let new_val = []
+				while (true) {
+					let b = message[index_update]
+					index_update += 1
+					if (b == undefined) {break}
+
+					//replace
+					if (b == 0){
+						const start = from_be_bytes(message.slice(index_update, index_update + 4))
+						const stop = from_be_bytes(message.slice(index_update +4, index_update + 8))
+						index_update += 8
+
+						const [field] = render_update.ob.item.u8.filter( field => {
+							let count = 0
+							while (true){
+								if (field[0][count] == undefined) {break}
+								if (key[count] == undefined) {break}
+								if (field[0][count] != key[count]) {return}
+								count += 1
+							}
+							return true
+						})
+
+						new_val = [
+							...field[1].slice(0, start),
+							...message.slice(index_update, index_update + stop-start),
+							...field[1].slice(stop, -1)
+						]
+
+					//insert
+					} else if (b == 1){
+						const start = from_be_bytes(message.slice(index_update, index_update + 4))
+						const stop = from_be_bytes(message.slice(index_update +4, index_update + 8))
+						index_update += 8
+
+
+					//delete
+					} else if (b == 2){
+						const start = from_be_bytes(message.slice(index_update, index_update + 4))
+						const stop = from_be_bytes(message.slice(index_update +4, index_update + 8))
+						index_update += 8
+
+
+					} else {break}
+					break
+
+					index_update += 1
+				}
+				i +=1
+
+				//u8
+				for (let i = 0; i < new_item.u8.length; i++){
+					while (true){
+						if (new_item.u8[i][0][count] == undefined) {break}
+						if (key[count] == undefined) {break}
+						if (new_item.u8[i][0][count] != key[count]) {new_item.u8[i][1] = new_val}
+						count += 1
+					}
+					pr("after while loop")
+					pr("in between: ", new_item)
+				}
+				pr("finished: ", new_item)
+				//str
+
+			}
+			pr(new_item.u8)
+			render_update.ob.updatedItemCallback(new_item)
 			break;
 
 		case 11:
@@ -121,16 +251,6 @@ async function handle_message(message){
 	//const number_array = await message.data.arrayBuffer()
 	//let arr = new Uint8Array(number_array)
 }
-
-document.addEventListener("DOMContentLoaded", () =>{
-	const so = new WebSocket("ws://localhost:3000/$api/socket")
-	so.onopen = () => {
-		so.onmessage = async (message) => {
-			handle_message(new Uint8Array(await message.data.arrayBuffer()))
-		}
-		main(so)
-	}
-})
 
 function from_be_bytes(bytes){
 	clone = Array.from(bytes)
@@ -193,5 +313,4 @@ function u32_to_be_bytes(num){
 
 	return new Uint8Array(bytes.reverse())
 }
-
 
